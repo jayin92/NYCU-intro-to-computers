4c2434be227d893e6b19f4c3b5f1bae09d0e37b6
jayin92 <jayin920805@gmail.com>
2021-10-18 13:38:27 +0800

    fix output

--- week4/110550088_hw_02.cpp
+++ week4/110550088_hw_02.cpp
@@ -2,22 +2,23 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-bool check(int n){
+bool check(int n) {
     int res = 0;
-    for(int i=1;i<n;i++){
-        if(n % i == 0){
+    for (int i = 1; i < n; i++) {
+        if (n % i == 0) {
             res += i;
         }
     }
     return res == n;
 }
 
-int main(){
+int main() {
     int n;
     printf("Please input an integer: ");
     scanf("%d", &n);
-    for(int i=1;i<=n;i++){
-        if(check(i)){
+    printf("All perfect numebers which is less than this integer:\n");
+    for (int i = 1; i <= n; i++) {
+        if (check(i)) {
             printf("%d\n", i);
         }
     }
