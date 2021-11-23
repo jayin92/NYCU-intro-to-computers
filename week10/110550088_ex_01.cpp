b46509749a2c45d02700b4a7a892a5990c71765e
class-1 <class-1@PCAD.CS.NCTU.EDU.TW>
2021-11-23 19:03:18 +0800

    fix after demo

--- week10/110550088_ex_01.cpp
+++ week10/110550088_ex_01.cpp
@@ -2,35 +2,42 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int sum(int a[20], int pos){
-    if(pos == 0){
-        return a[0];
-    } else {
-        return a[pos] + sum(a, pos-1);
+int sum(int a[20], int pos, int res) {
+    if (pos == 0) {
+        return res+a[pos];
+    }
+    else {
+        return sum(a, pos - 1, res+a[pos]);
     }
 }
 
-int main(){
-    int pos;
-    int a[20];
-    int sz = 0;
-    printf("Please input an array (size less than 10): ");
-    do{
-        scanf("%d", a+sz);
-        sz ++;
-    } while(getchar() != '\n');
-    
-    printf("How many elements to sum from head: ");
-    scanf("%d", &pos);
-    if(pos > sz){
-        printf("Out of range.\n");
-    } else {
-        if(pos == 0){
-            printf("The summation is 0.\n");
-        } else {
-            printf("The summation is %d.\n", sum(a, pos-1));
+int main() {
+    while (true) {
+        int pos;
+        int a[20];
+        int sz = 0;
+        printf("Please input an array(size less than 10): ");
+        do {
+            scanf("%d", a + sz);
+            sz++;
+        } while (getchar() != '\n');
+
+        printf("How many elements to sum from head: ");
+        scanf("%d", &pos);
+        if (pos > sz) {
+            printf("Out of range.\n");
         }
+        else {
+            if (pos == 0) {
+                printf("The summation is 0.\n");
+            }
+            else {
+                printf("The summation is %d.\n", sum(a, pos - 1, 0));
+            }
+        }
+        printf("\n");
     }
+
     system("pause");
     return 0;
 }
--- week10/110550088_ex_02.cpp
+++ week10/110550088_ex_02.cpp
@@ -2,34 +2,37 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int binary_search(int a[20], int l, int r, int tar){
-    if(l > r) return -1;
-    int mid = (l+r) >> 1;
-    if(a[mid] > tar){
-        return binary_search(a, l, mid-1, tar);
-    } else if(a[mid] < tar){
-        return binary_search(a, mid+1, r, tar);
-    } else {
+int binary_search(int a[20], int l, int r, int tar) {
+    if (l > r) return -1;
+    int mid = (l + r) / 2;
+    if (a[mid] > tar) {
+        return binary_search(a, l, mid - 1, tar);
+    }
+    else if (a[mid] < tar) {
+        return binary_search(a, mid + 1, r, tar);
+    }
+    else {
         return mid;
     }
 }
 
-int main(){
-    while(true){
+int main() {
+    while (true) {
         int a[20];
         int sz = 0;
         printf("Please input a sorted array(size less than 10): ");
-        do{
-            scanf("%d", a+sz);
-            sz ++;
-        } while(getchar() != '\n');
+        do {
+            scanf("%d", a + sz);
+            sz++;
+        } while (getchar() != '\n');
         int tar;
         printf("Please input the number you want to find: ");
         scanf("%d", &tar);
-        int ans = binary_search(a, 0, sz+1, tar);
-        if(ans == -1){
+        int ans = binary_search(a, 0, sz, tar);
+        if (ans == -1) {
             printf("Could not find the number.\n");
-        } else {
+        }
+        else {
             printf("The number is at the index %d.\n", ans);
         }
         printf("\n");
